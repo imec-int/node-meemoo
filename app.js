d46965e3430d6bb0d7e8dd08b38278cf3e7195e0
Sam Decrock <sam.decrock@gmail.com>
2013-05-07 10:27:33 +0200

    proxy tussen profile image gestoken

--- app.js
+++ app.js
@@ -129,73 +129,18 @@ app.get('/start', function (req, res){
 	io.sockets.emit('start', {});
 });
 
-// Proxy url for Twitter images
-app.get('/prox', function(request_from_client, response_to_client){
-	var url_parts = url.parse(request_from_client.url, true);
-	var query = url_parts.query;
-
-  
-  //var image_url = request_from_client.params.image_url;
-  var image_url = "http://"+query["image_url"];
-  console.log("Proxy request: "+image_url);
-
-	
+// Proxy url for Twitter images (eigenlijk voor alles :p)
+app.get('/prox', function (req, res){
+	if(!req.query.url) return res.json({err: "no url given"});
 
 
+	httpreq.get( req.query.url , {binary: true}, function (err, httpres){
+		if (err) return res.json({err: "error getting url", error: err});
 
-  var image_host_name = url.parse(image_url).hostname;
-  var image_path = url.parse(image_url).pathname;
-  var filename = url.parse(image_url).pathname.split("/").pop();
- 
-  	var options = {
-	  host: image_host_name,
-	  port: 80,
-	  path: image_path
-	};
-
-	var imgGet = http.get(options, function(resp){
-		//console.log(resp);
-		var current_byte_index = 0;
-		var response_content_length = parseInt(resp.headers["content-length"]);
-    	var response_body = new Buffer(response_content_length);
-		console.log("------"+resp.headers["content-length"]+"-------");
-	  resp.on('data', function(chunk){
-	  	//console.log("Datachunk: "+chunk);
-	    response_body.write(""+chunk, current_byte_index, "binary");
-      	current_byte_index += chunk.length;
-	  }).on('end',function(){
-	  	response_to_client.contentType(resp.headers["content-type"]);
-		//	response_to_client.body = response_body;
-      	response_to_client.send(response_body);
-      	console.log(response_to_client);
-	  	console.log("------ end --------");
-	  });
-	  	
+		// content type dorogeven
+		res.setHeader("Content-Type", httpres.headers['content-type']);
+		res.send(httpres.body);
 	});
-	imgGet.end();
-
-  console.log("host: "+image_host_name);
-  console.log("path: "+url.parse(image_url).pathname);
-  console.log("file: "+filename);
-
-  /*var http_client = http.createClient(80, image_host_name);
-  var image_get_request = http_client.request('GET', image_url, {"host": image_host_name});
-  image_get_request.addListener('response', function(proxy_response){
-    var current_byte_index = 0;
-    var response_content_length = parseInt(proxy_response.header("Content-Length"));
-    var response_body = new Buffer(response_content_length);
-   
-    proxy_response.setEncoding('binary');
-    proxy_response.addListener('data', function(chunk){
-      response_body.write(chunk, current_byte_index, "binary");
-      current_byte_index += chunk.length;
-    });
-    proxy_response.addListener('end', function(){
-      response_to_client.contentType(filename);
-      response_to_client.send(response_body);
-    });
-  });
-  image_get_request.end();*/
 });
 
 
@@ -338,7 +283,7 @@ function startTwitterhose(){
 			console.log("twitterhose started");
 			res.setEncoding('utf8');
 			res.addListener('data', function (chunk){
-				
+
 
 				cntr++;
 				//console.log("");
@@ -358,7 +303,7 @@ function startTwitterhose(){
 						}else{
 							//console.log("Delete status received");
 						}
-						
+
 					};
 				}catch(err){
 					console.log("Corrupt Tweet received: "+err.message);
@@ -375,6 +320,8 @@ function startTwitterhose(){
 }
 
 function addTweet(tweet){
+	// profile pic url vervangen:
+	tweet.user.profile_image_url = '/prox?url=' + encodeURIComponent(tweet.user.profile_image_url);
 	io.sockets.emit('newtweet', tweet);
 }
 
