dfecfcbcf6c975f01146ccf2425da7c116e06e99
Robby Wauters <robbywauters@gmail.com>
2013-05-10 13:48:14 +0200

    Vis: Receiving color (RGB) from Tweets

--- public/js/packery.js
+++ public/js/packery.js
@@ -4,7 +4,7 @@ $(function(){
 	// init socket
 	socket = io.connect(window.location.hostname);
 	// listen to socket
-	socket.on('newtweet', onSocket);
+	socket.on('colors', onSocket);
 
 	var container = document.querySelector('#container');
 	var pckry = new Packery( container, {
@@ -16,4 +16,18 @@ $(function(){
 
 function onSocket(data){
 	console.log(data);
+	var max = Math.max(data["red"],data["green"], data["blue"]);
+	var ratio = max/255 +0.1;
+
+	var nR = Math.floor(data["red"]/ratio);
+	var nG = Math.floor(data["green"]/ratio);
+	var nB = Math.floor(data["blue"]/ratio);
+
+	$("#red").text(nR);
+	$("#green").text(nG);
+	$("#blue").text(nB);
+
+console.log("rgb("+nR+","+nG+","+nB+")");
+	// bg color
+	$("body").css("background-color","rgb("+nR+","+nG+","+nB+")");
 }
