2bc9947d7d34e75b5b3bb95aa1b3947fc5b81e69
kaosbeat <kasper.jordaens@gmail.com>
2013-05-10 14:59:32 +0200

    dat.gui works in /d3

--- public/js/deedrie.js
+++ public/js/deedrie.js
@@ -10,3 +10,28 @@ $(function(){
 function onSocket(data){
 	console.log(data);
 }
+
+
+///DAT.gui stuff
+
+var params = {
+    interation: 5000
+};
+
+var FizzyText = function() {
+  this.speed = 0.8;
+  this.dataHoseToggle = false;
+  this.dataHose = function() {
+	speed = 50;
+	console.log(this.speed);
+	};
+};
+
+
+window.onload = function() {
+  var text = new FizzyText();	
+  var gui = new dat.GUI();
+  gui.add(params, 'interation');
+  gui.add(text, 'speed');
+
+};
