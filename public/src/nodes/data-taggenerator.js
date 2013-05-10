fc5c84b843349b4b778aa6b3a34a9f03530f7ffa
Robby Wauters <robbywauters@gmail.com>
2013-05-10 11:32:25 +0200

    Temporary keyword grabber function if NER fails

--- public/src/nodes/data-taggenerator.js
+++ public/src/nodes/data-taggenerator.js
@@ -3,11 +3,11 @@
 
 
 $(function(){
-
+  var tagsId = "text"+Date.now();
   var template =
     '<form class="textform">'+
       '<label><span class="label"><b>Tags</b></span> <br />'+
-        '<input type="text" class="tags" style="width:90%"></input>'+
+        '<input type="text" id="'+tagsId+'" style="width:90%"></input>'+
       '</label>'+
     '</form>';
 
@@ -27,10 +27,20 @@ $(function(){
         if(data.error) console.log(data.error);
         else{
           data = JSON.parse(data).join(',');
-          $('.tags').val(data);
+          $('#'+tagsId).val(data);
           that.send('tags', data);
+          return false;
         }
       });
+      
+      // temp: return random word
+      var terms = text.split(" ");
+      if (terms.length>0){
+
+        var rnd = Math.floor(Math.random()*terms.length);
+        $('#'+tagsId).val(terms[rnd]);
+        that.send('tags', terms[rnd]);
+      }
     },
 
     inputs: {
